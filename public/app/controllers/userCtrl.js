5ea4b7ef18f301eafaf458932cf352c9e2968cb8
ag251994 <ag251994@gmail.com>
2016-12-04 12:52:25 +0530

    minor UI changes

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -4,13 +4,13 @@ angular.module('userCtrl', ['userService'])
 .controller('UserController', function(User) {
 
 
-	var vm = this;
+    var vm = this;
 
-	
-	User.all()
-		.success(function(data) {
-			vm.users = data;
-		})
+
+    User.all()
+        .success(function(data) {
+            vm.users = data;
+        })
 
 
 })
@@ -18,19 +18,28 @@ angular.module('userCtrl', ['userService'])
 
 .controller('UserCreateController', function(User, $location, $window) {
 
-	var vm = this;
+    var vm = this;
 
-	vm.signupUser = function() {
-		vm.message = '';
+    vm.signupUser = function() {
+        vm.message = '';
+        if (validator.isEmail(vm.userData.email)) {
+            User.create(vm.userData)
+                .then(function(response) {
+                    vm.userData = {};
+                    vm.message = response.data.message;
+                    console.log(response.data.code)
+                    if (response.data.code == 11000) {
+                        vm.error = "Email Exist"
+                    } else {
+                        $window.localStorage.setItem('token', response.data.token);
+                        $location.path('/');
+                    }
+                })
+        } else {
+            vm.error = "Check Your Details"
 
-		User.create(vm.userData)
-			.then(function(response) {
-				vm.userData = {};
-				vm.message = response.data.message;
+        }
 
-				$window.localStorage.setItem('token', response.data.token);
-				$location.path('/');
-			})
-	}
+    }
 
 })
